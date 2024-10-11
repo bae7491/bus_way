e9f06e909e515b10138a902d041da136a91765ab
bae7491 <bae7491@naver.com>
2024-10-11 10:15:57 +0900

    버스 정류장 정보 & 관광지 정보를 보여줄 모달 창 추가

--- lib/ui/mainpage/main_map/main_map_view.dart
+++ lib/ui/mainpage/main_map/main_map_view.dart
@@ -24,10 +24,11 @@ class MainMapView extends StatelessWidget {
                     mainMapViewModel.moveCameraToCurrentLocation();
                   }
                 },
-                markers: mainMapViewModel.markers.toList(),
                 onMarkerTap: ((markerId, latLng, zoomLevel) {
-                  // TODO: 마커를 탭했을 때, 모달 창이 나오도록 해야함.
+                  // 마커를 탭했을 때, 모달 창 나오는 함수 불러오기
+                  showCustomModalBottomSheet(context);
                 }),
+                markers: mainMapViewModel.markers.toList(),
                 center: mainMapViewModel.center,
               ),
             ],
@@ -69,6 +70,106 @@ class MainMapView extends StatelessWidget {
             ],
           ),
           floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        );
+      },
+    );
+  }
+
+  // 마커를 클릭하면 모달 창이 보이는 함수
+  void showCustomModalBottomSheet(BuildContext context) {
+    // tabBar의 높이와 전체 화면의 높이를 계산하는 변수들
+    const double tabBarHeight = 90.0; // TabBar의 높이 설정
+    final double maxHeight =
+        MediaQuery.of(context).size.height * 0.8; // 화면의 80%
+
+    showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      useSafeArea: true,
+      builder: (context) {
+        return DraggableScrollableSheet(
+          expand: false,
+          initialChildSize: 0.5,
+          minChildSize: 0.1,
+          maxChildSize: 0.8,
+          builder: (context, scrollController) {
+            return Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              child: SingleChildScrollView(
+                controller: scrollController,
+                physics: const ClampingScrollPhysics(),
+                child: DefaultTabController(
+                  length: 2,
+                  initialIndex: 0,
+                  child: Column(
+                    children: [
+                      // 드래그 핸들러처럼 보이는 디자인 요소
+                      Container(
+                        margin: const EdgeInsets.only(top: 8.0),
+                        width: 50.0,
+                        height: 3.0,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      // TabBar 추가
+                      const PreferredSize(
+                        preferredSize:
+                            Size.fromHeight(tabBarHeight), // TabBar의 높이 설정
+                        child: TabBar(
+                          labelColor: Colors.black,
+                          indicatorColor: Colors.black,
+                          tabs: <Widget>[
+                            Tab(text: '버스'),
+                            Tab(text: '관광'),
+                          ],
+                        ),
+                      ),
+                      // TabBarView 추가
+                      SizedBox(
+                        height: maxHeight -
+                            tabBarHeight, // TabBar height를 제외한 나머지 공간 설정
+                        child: TabBarView(
+                          children: <Widget>[
+                            // 첫 번째 탭: 버스 정류장 정보
+                            ListView.builder(
+                              itemCount: 10, // 예시 데이터
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  leading: const Icon(Icons.directions_bus),
+                                  title: Text('버스 정류장 $index'),
+                                  subtitle: Text('다음 버스 도착: ${index * 2}분'),
+                                );
+                              },
+                            ),
+                            // 두 번째 탭: 관광 정보
+                            ListView.builder(
+                              itemCount: 10, // 예시 데이터
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  leading: const Icon(Icons.place),
+                                  title: Text('관광지 $index'),
+                                  subtitle: Text('관광지 설명 $index'),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
         );
       },
     );
