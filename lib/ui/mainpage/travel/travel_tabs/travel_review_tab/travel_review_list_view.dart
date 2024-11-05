66423b6a37c020fa6a64ac1f5e82545404dddc00
bae7491 <bae7491@naver.com>
2024-11-05 10:58:09 +0900

    관광지 상세 - 리뷰 불러오는 페이지의 불필요 코드 제거

--- lib/ui/mainpage/travel/travel_tabs/travel_review_tab/travel_review_list_view.dart
+++ lib/ui/mainpage/travel/travel_tabs/travel_review_tab/travel_review_list_view.dart
@@ -4,109 +4,103 @@ import 'package:bus_way/ui/mainpage/travel/travel_review/travel_review_view.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 
 Widget travelReviewListView(
     BuildContext context, TravelDetailViewModel viewmodel, String contentId) {
-  return Consumer<TravelDetailViewModel>(
-    builder: (context, viewmodel, child) {
-      final reviewInfo = viewmodel.travelReviewInfo;
-
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 30.0,
-              vertical: 8.0,
-            ),
-            child: Column(
-              children: [
-                if (reviewInfo != null)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  final reviewInfo = viewmodel.travelReviewInfo;
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30.0,
+          vertical: 8.0,
+        ),
+        child: Column(
+          children: [
+            if (reviewInfo != null)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
                     children: [
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          // 전체 후기 개수
+                          Text(
+                            '전체 (${NumberFormat('###,###,###,###').format(
+                              int.parse(reviewInfo.reviewCount!),
+                            )} 건)',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          // 별점
+                          Row(
                             children: [
-                              // 전체 후기 개수
                               Text(
-                                '전체 (${NumberFormat('###,###,###,###').format(
-                                  int.parse(reviewInfo.reviewCount!),
-                                )} 건)',
+                                reviewInfo.reviewAverageRate!,
                                 style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
                                   fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
                                 ),
                                 textAlign: TextAlign.start,
                               ),
-                              // 별점
-                              Row(
-                                children: [
-                                  Text(
-                                    reviewInfo.reviewAverageRate!,
-                                    style: const TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    textAlign: TextAlign.start,
-                                  ),
-                                  const SizedBox(width: 10.0),
-                                  RatingBarIndicator(
-                                    rating: double.parse(
-                                        reviewInfo.reviewAverageRate!),
-                                    direction: Axis.horizontal,
-                                    itemCount: 5,
-                                    itemSize: 30,
-                                    itemBuilder: (context, _) => const Icon(
-                                      Icons.star_rate,
-                                      color: Colors.amber,
-                                    ),
-                                  ),
-                                ],
+                              const SizedBox(width: 10.0),
+                              RatingBarIndicator(
+                                rating:
+                                    double.parse(reviewInfo.reviewAverageRate!),
+                                direction: Axis.horizontal,
+                                itemCount: 5,
+                                itemSize: 30,
+                                itemBuilder: (context, _) => const Icon(
+                                  Icons.star_rate,
+                                  color: Colors.amber,
+                                ),
                               ),
                             ],
                           ),
                         ],
                       ),
-                      // 후기 작성 버튼
-                      ElevatedButton(
-                        onPressed: () async {
-                          await Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => TravelReviewView(
-                                contentId: contentId,
-                              ),
-                            ),
-                          );
-                          // 두 번째 화면에서 돌아오면 getTravelReview 호출하여 새로고침
-                          await viewmodel.getTravelReview(contentId);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: orchid,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        child: const Text(
-                          "후기 작성",
-                        ),
-                      ),
                     ],
                   ),
-                const Divider(
-                  height: 30,
-                  thickness: 5,
-                ),
-              ],
+                  // 후기 작성 버튼
+                  ElevatedButton(
+                    onPressed: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TravelReviewView(
+                            contentId: contentId,
+                          ),
+                        ),
+                      );
+                      // 두 번째 화면에서 돌아오면 getTravelReview 호출하여 새로고침
+                      await viewmodel.getTravelReview(contentId);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: orchid,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      "후기 작성",
+                    ),
+                  ),
+                ],
+              ),
+            const Divider(
+              height: 30,
+              thickness: 5,
             ),
-          ),
-        ],
-      );
-    },
+          ],
+        ),
+      ),
+    ],
   );
 }
